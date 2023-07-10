eed7a5eef1456b7054e9a43047931887971dc405
aagun <agun@duck.com>
2023-07-10 17:25:51 +0700

    fixed mandatory field

--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -4,7 +4,7 @@ const SongPayloadSchema = Joi.object({
   title: Joi.string().required().max(255),
   year: Joi.number().required().integer().positive(),
   genre: Joi.string().required().max(50),
-  performer: Joi.string().max(255),
+  performer: Joi.string().required().max(255),
   duration: Joi.number().integer().positive(),
   albumId: Joi.string().max(32),
 });
